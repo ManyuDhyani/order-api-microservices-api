ade1db986a871b151335da11bdedf85c927324df
ManyuDhyani <manyudhyani@gmail.com>
2024-06-23 20:28:31 -0400

    chi router and middleware added

--- main.go
+++ main.go
@@ -3,16 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 // main is the entry point of the program
 func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/hello", basicHandler)
+
 	// Create a new HTTP server
 	server := &http.Server{
 		// The address to listen on, in this case port 3000 on all network interfaces
 		Addr: ":3000",
 		// The handler for incoming requests, here we use the basicHandler function
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	// Start the server and listen for incoming HTTP requests
@@ -25,6 +32,6 @@ func main() {
 
 // basicHandler handles incoming HTTP requests
 func basicHandler(w http.ResponseWriter, r *http.Request) {
-	// Write "Hello, world!" to the response body
+	// Write "Server is  Live" to the response body
 	w.Write([]byte("Server is Live"))
 }
